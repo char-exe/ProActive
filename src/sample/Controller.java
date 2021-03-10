22001ceae8dd326c55cd31cc7199c82c70023d95
Andrew-Storey <57496858+Andrew-Storey@users.noreply.github.com>
2021-03-10 19:53:51 +0000

    Created the basic form, controller still needs work.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,13 @@
-package sample;
+package RegForm;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+import javax.swing.*;
 
 public class Controller {
+    @FXML protected void handleSubmitButtonAction(ActionEvent event) {
+        System.out.println("Button Pressed!");
+    }
 }
