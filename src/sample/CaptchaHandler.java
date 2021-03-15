6ce02f40250260e780b02a8d66f7527cf728d98f
Bajeebusses <57506193+Bajeebusses@users.noreply.github.com>
2021-03-15 10:20:48 +0000

    Captcha Handler

--- src/sample/CaptchaHandler.java
+++ src/sample/CaptchaHandler.java
@@ -3,18 +3,19 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.Image;
 
 import java.awt.*;
+import java.io.*;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Random;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 import java.util.ResourceBundle;
 import javax.imageio.ImageIO;
 
@@ -29,36 +30,49 @@ public class CaptchaHandler implements Initializable {
 
     //random number generator for selecting a random index
     private Random generator = new Random();
-    private String captcha;
 
-    //constructor, this gets called before any FXML so it only populates the ArrayList
+
+    //empty constructor, all the main work is done in initialize
     public CaptchaHandler() {
-        for(String captcha:tempArray){
-            captchaList.add(captcha);
-        }
-        //int captchaIndex = generator.nextInt(captchaList.size());
-        int captchaIndex = 1;
-        String captcha = captchaList.get(captchaIndex);
-        System.out.println(captcha);
     }
 
-
+    private String captcha;
     @FXML ImageView captchaImageBox;
     @FXML TextField captchaInput;
     @FXML Label incorrectInputLabel;
 
+    public String serveCaptcha() {
+        int captchaIndex = generator.nextInt(captchaList.size());
+        String captcha = captchaList.get(captchaIndex);
+        System.out.println(captcha);
+
+        //displays captcha image
+        File file = new File("src/sample/CaptchaImages/"+captcha+".JPG");
+        InputStream captchaFileStream = null;
+        try {
+            captchaFileStream = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Image captchaImage = new Image(captchaFileStream);
+        captchaImageBox.setImage(captchaImage);
+        return captcha;
+    }
+
     //this is executed after the FXML elements are initialized and thus the image is served here
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        File file = new File("src/CaptchaImages/"+captcha+".jpg");
-        Image captchaImage = new Image(file.toURI().toString());
-        captchaImageBox.setImage(captchaImage);
+        for(String captchaTemp:tempArray){
+            captchaList.add(captchaTemp);
+        }
+        captcha = serveCaptcha();
     }
 
     //checks to see if user's input matches the captcha content
     public void validateCaptcha(ActionEvent actionEvent) {
         System.out.println(captchaInput.getText());
         String input = captchaInput.getText().stripTrailing().stripLeading().stripIndent();
+
 
         //redirect if valid
         if (input.equals(captcha)) {
@@ -67,6 +81,7 @@ public class CaptchaHandler implements Initializable {
         //display error if invalid
         else {
             incorrectInputLabel.setText("Incorrect answer, try again.");
+            captcha = serveCaptcha();
         }
     }
 
