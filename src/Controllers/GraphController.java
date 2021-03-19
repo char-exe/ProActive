05a6700842bfba58bb34b137c36a8820b8114c01
psb18 <79208417+psb18@users.noreply.github.com>
2021-03-19 19:36:39 +0000

    Wrote Javadoc and code comments for GraphController.

--- src/Controllers/GraphController.java
+++ src/Controllers/GraphController.java
@@ -8,98 +8,88 @@ import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.util.StringConverter;
 
+import java.io.IOException;
 import java.net.URL;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
 
+/**
+ * A class to control the summary viewing page of the ProActive App. Handles the adding of data to the graphs and the
+ * formatting of x axes from number to date format.
+ *
+ * @author Samuel Scarfe 100048633
+ * @version 1.0 - Simple graphing with a loop for simple dummy data and axis formatting.
+ */
 public class GraphController implements Initializable
 {
-    @FXML
-    private NumberAxis burnDateAxis;
-    @FXML
-    private NumberAxis intakeDateAxis;
-    @FXML
-    private NumberAxis spentDateAxis;
+    /**
+     * A chart for viewing caloric intake over 7 days.
+     */
     @FXML
     private LineChart<Number, Number> intakeChart;
+    /**
+     * A chart for viewing caloric burn over 7 days.
+     */
     @FXML
     private LineChart<Number, Number> burnChart;
+    /**
+     * A chart for view time spent exercising over 7 days.
+     */
     @FXML
     private LineChart<Number, Number> spentChart;
+    /**
+     * The x axis for intakeChart.
+     */
+    @FXML
+    private NumberAxis intakeDateAxis;
+    /**
+     * The x axis for burnChart.
+     */
+    @FXML
+    private NumberAxis burnDateAxis;
+    /**
+     * The x axis for spentChart.
+     */
+    @FXML
+    private NumberAxis spentDateAxis;
 
-    public GraphController(){
+    /**
+     * Class constructor. Loads the Graph.fxml file into the FXMLLoader and sets this as the controller.
+     */
+    public GraphController()
+    {
+        /*
         FXMLLoader loader = new FXMLLoader(getClass().getResource("../FXML/Graph.fxml"));
         loader.setController(this);
-        try {
+        try
+        {
             loader.load();
         }
-        catch(Exception e)
+        catch(IOException e)
         {
             System.out.println(e.getMessage() + "Error loading Graph.fxml");
         }
+
+         */
     }
 
+    /**
+     * Initializes the graphs with formatted axes and dummy data.
+     *
+     * @param url The location used to resolve relative paths for the root object, or null if the location is not known.
+     * @param resourceBundle The resources used to localize the root object, or null if the root object was not localized.
+     * @see javafx.fxml.Initializable
+     */
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+        //format each axis from showing numeric tick marks to showing the past 7 days
+        formatAxis(intakeDateAxis);
+        formatAxis(burnDateAxis);
+        formatAxis(spentDateAxis);
 
-        intakeDateAxis.setTickLabelFormatter(new StringConverter<>() {
-            @Override
-            public String toString(Number number)
-            {
-                //https://stackoverflow.com/questions/11882926/how-to-subtract-x-day-from-a-date-object-in-java
-                return LocalDate.now().minusDays(7 - number.intValue()).toString();
-            }
-
-            //https://stackoverflow.com/questions/13037654/subtract-two-dates-in-java
-            @Override
-            public Number fromString(String s) {
-                LocalDate d1 = LocalDate.now();
-                LocalDate d2 = LocalDate.parse(s);
-                Duration diff = Duration.between(d1.atStartOfDay(), d2.atStartOfDay());
-
-                return diff.toDays();
-            }
-        });
-
-        burnDateAxis.setTickLabelFormatter(new StringConverter<>() {
-            @Override
-            public String toString(Number number)
-            {
-                //https://stackoverflow.com/questions/11882926/how-to-subtract-x-day-from-a-date-object-in-java
-                return LocalDate.now().minusDays(7 - number.intValue()).toString();
-            }
-
-            //https://stackoverflow.com/questions/13037654/subtract-two-dates-in-java
-            @Override
-            public Number fromString(String s) {
-                LocalDate d1 = LocalDate.now();
-                LocalDate d2 = LocalDate.parse(s);
-                Duration diff = Duration.between(d1.atStartOfDay(), d2.atStartOfDay());
-
-                return diff.toDays();
-            }
-        });
-
-        spentDateAxis.setTickLabelFormatter(new StringConverter<>() {
-            @Override
-            public String toString(Number number)
-            {
-                //https://stackoverflow.com/questions/11882926/how-to-subtract-x-day-from-a-date-object-in-java
-                return LocalDate.now().minusDays(7 - number.intValue()).toString();
-            }
-
-            //https://stackoverflow.com/questions/13037654/subtract-two-dates-in-java
-            @Override
-            public Number fromString(String s) {
-                LocalDate d1 = LocalDate.now();
-                LocalDate d2 = LocalDate.parse(s);
-                Duration diff = Duration.between(d1.atStartOfDay(), d2.atStartOfDay());
-
-                return diff.toDays();
-            }
-        });
-
+        //Create a data series for each graph and set names
         XYChart.Series<Number, Number> intakeSeries = new XYChart.Series<>();
         XYChart.Series<Number, Number> burnSeries = new XYChart.Series<>();
         XYChart.Series<Number, Number> spentSeries = new XYChart.Series<>();
@@ -108,26 +98,75 @@ public class GraphController implements Initializable
         burnSeries.setName("Calories Burned via Exercise");
         spentSeries.setName("Minutes Spent");
 
+        //Create a datapoint for each day.
         for (int i = 1; i < 8; i++)
         {
+            //Add data points to series.
             intakeSeries.getData().add(new XYChart.Data<>(i, i));
             burnSeries.getData().add(new XYChart.Data<>(i, i));
             spentSeries.getData().add(new XYChart.Data<>(i, i));
         }
 
+        //Add series to graphs.
         intakeChart.getData().add(intakeSeries);
         burnChart.getData().add(burnSeries);
         spentChart.getData().add(spentSeries);
 
+        /*
         FXMLLoader loader = new FXMLLoader(getClass().getResource("../FXML/Graph.fxml"));
         loader.setController(this);
-        try {
+        try
+        {
             loader.load();
         }
-        catch(Exception e)
+        catch(IOException e)
         {
             System.out.println(e.getMessage() + "Error loading Graph.fxml");
         }
+
+         */
+    }
+
+    /**
+     * Private method for formatting a NumberAxis to show dates for the past week instead of ordinals.
+     * Centers today's date at ordinal 7.
+     *
+     * @param DateAxis A NumberAxis object for a JavaFX Chart
+     */
+    private void formatAxis(NumberAxis DateAxis)
+    {
+        //Set tick labels by calling the formatter method and passing an anonymous StringConverter.
+        DateAxis.setTickLabelFormatter(new StringConverter<>()
+        {
+            /**
+             * Converts a Number to a String representing a date.
+             * @param number The numeric tick mark.
+             * @return A string representation of a date with 7 equal to today's date.
+             */
+            @Override
+            public String toString(Number number)
+            {
+                //https://stackoverflow.com/questions/11882926/how-to-subtract-x-day-from-a-date-object-in-java
+                return LocalDate.now().minusDays(7 - number.intValue()).toString();
+            }
+
+            /**
+             * Converts a String representation of a date to an ordinal by comparing the date to today.
+             *
+             * @param s A string representing a date.
+             * @return An ordinal representation of the date, 7 equals today.
+             */
+            //https://stackoverflow.com/questions/13037654/subtract-two-dates-in-java
+            @Override
+            public Number fromString(String s)
+            {
+                LocalDate d1 = LocalDate.now();
+                LocalDate d2 = LocalDate.parse(s);
+                Duration diff = Duration.between(d1.atStartOfDay(), d2.atStartOfDay());
+
+                return diff.toDays();
+            }
+        });
     }
 
 }
