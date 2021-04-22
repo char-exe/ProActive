02c7bd3e144100d306b923960d5673a1070759d3
psb18 <79208417+psb18@users.noreply.github.com>
2021-04-22 22:39:48 +0100

    Added Javadoc and exceptions.

--- src/sample/NutritionItem.java
+++ src/sample/NutritionItem.java
@@ -1,21 +1,30 @@
 package sample;
 
+/**
+ * A class to represent a nutrition item in the application as it appears in the database.
+ *
+ * @author Evan Clayton?
+ * @author Samuel Scarfe
+ *
+ * @version 1.1
+ *
+ * 1.0 - First working version.
+ * 1.1 - Added Javadoc and exceptions. Removed no argument constructor. Wrote JUnit test class.
+ */
 public class NutritionItem {
     private String name;
-    private double kcal, protein, fat, carbs, sugar, fibre, cholesterol;
-
-    //get methods
-    public String getName() { return name; }
-    public double getKcal() { return kcal; }
-    public double getProtein() { return protein; }
-    public double getFat() { return fat; }
-    public double getCarbs() { return carbs; }
-    public double getSugar() { return sugar; }
-    public double getFibre() { return fibre; }
-    public double getCholesterol() { return cholesterol; }
+    private double kcal;
+    private double protein;
+    private double fat;
+    private double carbs;
+    private double sugar;
+    private double fibre;
+    private double cholesterol;
 
     public NutritionItem(String name, double kcal, double protein, double fat,
                          double carbs, double sugar, double fibre, double cholesterol) {
+        checkConstructorInputs(name, kcal, protein, fat, carbs, sugar, fibre, cholesterol);
+
         this.name = name;
         this.kcal = kcal;
         this.protein = protein;
@@ -26,15 +35,67 @@ public class NutritionItem {
         this.cholesterol = cholesterol;
     }
 
-    public NutritionItem() {
-        this.name = "DefaultName";
-        this.kcal = 0.0;
-        this.protein = 0.0;
-        this.fat = 0.0;
-        this.carbs = 0.0;
-        this.sugar = 0.0;
-        this.fibre = 0.0;
-        this.cholesterol = 0.0;
+    //get methods
+    public String getName() {
+        return name;
     }
 
+    public double getKcal() {
+        return kcal;
+    }
+
+    public double getProtein() {
+        return protein;
+    }
+
+    public double getFat() {
+        return fat;
+    }
+
+    public double getCarbs() {
+        return carbs;
+    }
+
+    public double getSugar() {
+        return sugar;
+    }
+
+    public double getFibre() {
+        return fibre;
+    }
+
+    public double getCholesterol() {
+        return cholesterol;
+    }
+
+    private void checkConstructorInputs(String name, double kcal, double protein, double fat,
+                                        double carbs, double sugar, double fibre, double cholesterol) {
+        if (name == null) {
+            throw new NullPointerException();
+        }
+        if (name.length() < 1) {
+            throw new IllegalArgumentException();
+        }
+        if (kcal < 0) {
+            throw new IllegalArgumentException();
+        }
+        if (protein < 0) {
+            throw new IllegalArgumentException();
+        }
+        if (fat < 0) {
+            throw new IllegalArgumentException();
+        }
+        if (carbs < 0) {
+            throw new IllegalArgumentException();
+        }
+        if (sugar < 0) {
+            throw new IllegalArgumentException();
+        }
+        if (fibre < 0) {
+            throw new IllegalArgumentException();
+        }
+        if (cholesterol < 0) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
