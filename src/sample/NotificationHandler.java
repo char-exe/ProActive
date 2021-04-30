e0fce70733089929ca915acbbd6f2db97378f32b
Bajeebusses <57506193+Bajeebusses@users.noreply.github.com>
2021-04-30 18:44:20 +0100

    Singleton pattern for NotificationHandler. Methods for sending emails as well as displaying a notification. 2 additional notification display types (blink and fade) plus the corresponding method versions for the two new types.

--- src/sample/NotificationHandler.java
+++ src/sample/NotificationHandler.java
@@ -4,30 +4,54 @@ import Controllers.MainController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 
+import javax.mail.Session;
 import java.io.IOException;
+import java.util.Properties;
 
 /**
  * A class to be instantiated in other classes to allow for easy displaying of notifications for goals.
  *
  * @author Evan Clayton 100278720
  *
- * @version 1.0 Added the constructor and method needed for passing messages to the MainController to be displayed.
+ * @version 1.0 - Added the constructor and method needed for passing messages to the MainController to be displayed.
+ *          1.1 - Now follows a singleton pattern and added a method for displaying goal completion notifications and
+ *                sending an email at the same time.
+ *          1.2 - Additional methods for using the alternate blink and fade notification types.
  */
 public class NotificationHandler {
 
-    private MainController mainController;
+    private static final NotificationHandler INSTANCE = new NotificationHandler();
+
+
+
+    private static MainController mainController;
+    private static final EmailHandler emailHandler = EmailHandler.getInstance();
+    private static Session session;
 
     /**
      * Constructor that creates the link to the MainController so that the showNotification method can be called.
+     * Private to enforce singleton pattern
      *
      * @throws IOException
      */
-    public NotificationHandler() throws IOException {
+    private NotificationHandler() {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("Main.fxml"));
-        Parent root = loader.load();
+        try {
+            Parent root = loader.load(); //had to put in a try catch to avoid throwing IOException so that singleton pattern can be used
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         MainController controller = loader.<MainController>getController();
         this.mainController = controller;
+        Properties prop = emailHandler.SetUpEmailHandler();
+        this.session = emailHandler.createSession(prop);
     }
+
+    /**
+     * Static method to get the single instance of NotificationHandler
+     * @return the NotificationHandler instance
+     */
+    public static NotificationHandler getInstance() { return INSTANCE; }
 
     /**
      * Method that calls the showNotification method from the MainController
@@ -37,4 +61,62 @@ public class NotificationHandler {
     public void displayNotification(String message){
         mainController.showNotification(message);
     }
+
+    /**
+     * Method that calls the showBlinkNotification method from the MainController.
+     *
+     * @param message The text to be displayed.
+     */
+    public void displayBlinkNotification(String message){
+        mainController.showBlinkNotification(message);
+    }
+
+    /**
+     * Method that calls the showFadeNotification method from the MainController.
+     *
+     * @param message The text to be displayed.
+     */
+    public void displayFadeNotification(String message){
+        mainController.showFadeNotification(message);
+    }
+
+    /**
+     * Method that calls the showNotification method from the MainController as well as sending an email to the user
+     * informing them of the goal's completion.
+     *
+     * @param email Email to send the message to.
+     * @param goalName The name of the completed goal.
+     */
+    public void sendGoalNotificationWithEmail (String email, String goalName) {
+        String message = "You have completed the goal: " + goalName;
+        displayNotification(message);
+        emailHandler.sendGoalCompletion(session, email, goalName);
+    }
+
+    /**
+     * Method that calls the showBlinkNotification method from the MainController as well as sending an email to the user
+     * informing them of the goal's completion.
+     *
+     * @param email Email to send the message to.
+     * @param goalName The name of the completed goal.
+     */
+    public void sendGoalNotificationWithEmail_Blink (String email, String goalName) {
+        String message = "You have completed the goal: " + goalName;
+        displayBlinkNotification(message);
+        emailHandler.sendGoalCompletion(session, email, goalName);
+    }
+
+    /**
+     * Method that calls the showFadeNotification method from the MainController as well as sending an email to the user
+     * informing them of the goal's completion.
+     *
+     * @param email Email to send the message to.
+     * @param goalName The name of the completed goal.
+     */
+    public void sendGoalNotificationWithEmail_Fade (String email, String goalName) {
+        String message = "You have completed the goal: " + goalName;
+        displayFadeNotification(message);
+        emailHandler.sendGoalCompletion(session, email, goalName);
+    }
+
 }
